8ada6e0d4143bc50dbb9794710efea5cb7a8200d
ghostpert07 <jeffweb40@gmail.com>
2022-10-27 12:00:03 -0700

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,77 @@
-/*
- * File: 0-binary_to_uint.c
- * Author: Kefuoe Sole
- */
 #include "main.h"
+
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- * Otherwise - the converted number.
- */
+  *_strlen - returns the length of a string.
+  *@s: pointer to string.
+  *
+  *Return: length.
+*/
+
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+  *getp - returns the value of x to the power of y.
+  *@x: number.
+  *@y: power.
+  *
+  *Return: x to the pow of y.
+  *0 if x < 0.
+*/
+
+int getp(int x, int y)
+{
+	if (y < 0)
+		return (0);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+
+	return (x * getp(x, y - 1));
+}
+
+/**
+  *binary_to_uint - converts a binary number to an unsigned int.
+  *@b: pointer to string containing 0 and 1.
+  *
+  *Return: converted number or 0 if b is null or has chars not 0 or 1.
+*/
+
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0, mult = 1;
-int len;
+	unsigned int n, l, i;
+	int power;
 
-if (b == '\0')
-return (0);
+	if (!b)
+		return (0);
 
-for (len = 0; b[len];)
-len++;
-
-for (len -= 1; len >= 0; len--)
-{
-if (b[len] != '0' && b[len] != '1')
-return (0);
-
-num += (b[len] - '0') * mult;
-mult *= 2;
-}
-return (num);
+	l = _strlen(b);
+	power = 0;
+	n = 0;
+	i = l - 1;
+	while (l > 0)
+	{
+		if (b[i] == 48 || b[i] == 49)
+		{
+			n = n + ((b[i] - 48) * getp(2, power));
+			power++;
+			i--;
+			l--;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (n);
 }
